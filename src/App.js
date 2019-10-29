a1722a3a12a9bb20a785a694724104a70a30420c
John Jeong <jjohnvi@Johns-MacBook-Pro.local>
2019-10-29 09:53:23 -0500

    first com

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    open: false
+  };
+
+  toggleMenu = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  render() {
+    let sidebarClassnames = "sidemenu";
+    if (this.state.open) {
+      sidebarClassnames += " sidemenu--open";
+    }
+
+    return (
+      <div className="App">
+        <nav className="navbar">
+          <div className="navbar__title">Title</div>
+          <ul className="dropdown__list">
+            <li className="dropdown__item">Services</li>
+            <li className="dropdown__item">Portfolio</li>
+            <li className="dropdown__item">About</li>
+            <li className="dropdown__item">Team</li>
+            <li className="dropdown__item">Contact</li>
+          </ul>
+          <button className="navbar__menu" onClick={this.toggleMenu}>
+            Menu
+          </button>
+        </nav>
+
+        <div className={sidebarClassnames}>
+          <ul className="sidemenu__list">
+            <li className="sidemenu__item">Services</li>
+            <li className="sidemenu__item">Portfolio</li>
+            <li className="sidemenu__item">About</li>
+            <li className="sidemenu__item">Team</li>
+            <li className="sidemenu__item">Contact</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
